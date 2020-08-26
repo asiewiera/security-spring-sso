3602f1ebc97069774f0119faa802c10b171232a7
Agnieszka Siewiera <Agnieszka1.Siewiera@ge.com>
2020-08-26 12:24:05 +0200

    Initial commit

---
+++ src/main/java/com/ge/securityspringsso/SecuritySpringSsoApplication.java
@@ -0,0 +1,13 @@
+package com.ge.securityspringsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySpringSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ge/securityspringsso/SecuritySpringSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ge.securityspringsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
